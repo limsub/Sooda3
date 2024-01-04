f32d4a91b3348a893d03b9c8b6a519b769c37de1
limsss9076@naver.com <limsss9076@naver.com>
2024-01-04 22:16:35 +0900

    initial commit

---
+++ Sooda3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sooda3
+//
+//  Created by 임승섭 on 1/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
