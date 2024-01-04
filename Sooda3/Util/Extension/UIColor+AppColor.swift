33f9000451a2c9b45dd82ea16f8a74760be50049
limsss9076@naver.com <limsss9076@naver.com>
2024-01-04 22:26:20 +0900

    [Setting] 피그마 기본 색상 추가

---
+++ Sooda3/Util/Extension/UIColor+AppColor.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+AppColor.swift
+//  Sooda3
+//
+//  Created by 임승섭 on 1/4/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static func appColor(_ colorset: ColorSet) -> UIColor {
+        return UIColor(hexCode: colorset.hexCode)
+    }
+}
+
